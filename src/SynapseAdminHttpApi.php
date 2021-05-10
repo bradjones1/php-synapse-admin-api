a2582c98447840fc046ff79588fe8ecece2bc586
Brad Jones <brad.jones@fruition.net>
2021-05-09 21:15:38 -0700

    Don't require matrix sdk

---
+++ src/SynapseAdminHttpApi.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BradJones1\SynapseAdmin;
+
+/**
+ * Class SynapseAdminHttpApi
+ *
+ * @see https://github.com/matrix-org/synapse/tree/master/docs/admin_api
+ *
+ * @package BradJones1\SynapseAdmin
+ */
+class SynapseAdminHttpApi {
+}
